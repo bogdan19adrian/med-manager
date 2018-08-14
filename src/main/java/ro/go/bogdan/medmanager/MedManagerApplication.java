805915c084593172d21a75ba51cb19c986ed8424
bogdan.enache <bogdan.enache@asf.ro>
2018-08-14 15:01:26 +0300

    first commit

---
+++ src/main/java/ro/go/bogdan/medmanager/MedManagerApplication.java
@@ -0,0 +1,12 @@
+package ro.go.bogdan.medmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedManagerApplication.class, args);
+	}
+}
---
+++ src/test/java/ro/go/bogdan/medmanager/MedManagerApplicationTests.java
@@ -0,0 +1,16 @@
+package ro.go.bogdan.medmanager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MedManagerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
