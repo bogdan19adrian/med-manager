b106367cc9ab97050a35098bb137741fc64bf1cd
bogdan <bogdan10adrian@gmail.com>
2018-08-15 22:53:10 +0300

    added user class, controller, repository

--- src/main/resources/db/migrations/V2__alter_user_table.sql
+++ src/main/resources/db/migrations/V2__alter_user_table.sql
@@ -2,3 +2,8 @@ INSERT INTO users
     (id, active, email, first_name, last_name, password, user_name)
  VALUES
     (1, TRUE, 'bogdan19adrian@gmail.com', 'bogdan', 'enache', '123456', 'eba' );
+
+INSERT INTO users
+    (id, active, email, first_name, last_name, password, user_name)
+ VALUES
+    (2, TRUE, 'bogdan19adrian@gmail.com', 'bogdan2', 'enache2', '123456', 'eba' );
---
+++ src/main/resources/db/migrations/V3__another_insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO users
+    (id, active, email, first_name, last_name, password, user_name)
+ VALUES
+    (3, TRUE, 'bogdan19adrian@gmail.com', 'bogdan3', 'enache3', '123456', 'eba3' );
